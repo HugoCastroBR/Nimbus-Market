10914340c61a6ae8a6641c14e573b6bd22e70737
HugoCastroBr <hugocastrohc@outlook.com>
2024-10-08 19:28:19 -0300

    feat: settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,8 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
-            .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly.</li> 
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -45,6 +44,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+
             Read our docs
           </a>
         </div>
